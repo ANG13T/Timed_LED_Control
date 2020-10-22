e2c6204dc41fe4ea16c75136b6c02c04426616cf
angelina-tsuboi <angelina.t1832@gmail.com>
2020-10-22 10:56:23 -0700

    basic code functionality complete

--- timed_LED_control.ino
+++ timed_LED_control.ino
@@ -1,7 +1,7 @@
-const int red = 0;
-const int blue = 1;
-const int green = 2;
+const int red = 2;
 const int yellow = 3;
+const int blue = 4;
+const int green = 5;
 const int intervalPot = A0;
 const int amountPot = A1;
 
@@ -11,11 +11,12 @@ int amountValue; //amount of lights shining at each interval
 int value = 0; //num of light groups shining
 
 void setup() {
+  Serial.begin(9600);
   // put your setup code here, to run once:
-  pinMode(red, OUTPUT); //LEDS
+  pinMode(red, OUTPUT);
   pinMode(blue, OUTPUT);
-  pinMode(green, OUTPUT);
   pinMode(yellow, OUTPUT);
+  pinMode(green, OUTPUT);
   pinMode(intervalPot, INPUT); // potentiometer input values
   pinMode(amountPot, INPUT);
 }
@@ -26,15 +27,23 @@ void loop() {
   amountValue = analogRead(amountPot);
   intervalValue = map(intervalValue, 0, 1023, 0, 255); //Map value 0-1023 to 0-255 (PWM)
   amountValue = map(amountValue, 0, 1023, 0, 255); 
-
+//  Serial.print("intervalValue: ");
+//  Serial.println(intervalValue);
+//  Serial.print("amountValue: ");
+//  Serial.println(amountValue);
+//  updateLED(3);
+  updateLED(1);
+  delay(1000);
+  updateLED(4);
+  delay(1000);
 }
 
 void updateLED(int amount){
-  for(int i = 0; i < amount; i++){
-    digitalWrite(i, HIGH);
-  }
-
-  for(int i = 3; i >= amount; i--){
-     digitalWrite(i, LOW);
-  } 
+  for(int i = 0; i < 4; i++){ //loops through sections
+    if(i < amount){
+      digitalWrite(i + 2, HIGH);
+     }else{
+      digitalWrite(i + 2, LOW);
+     }
+   }
 }
